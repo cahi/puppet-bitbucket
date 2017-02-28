f421590f9324df0bb7533c635eb46052a55340ea
Carsten Hilber <chilber@agile-im.de>
2017-02-28 11:58:07 +0100

    commented facter

--- lib/facter/bitbucket_setup.rb
+++ lib/facter/bitbucket_setup.rb
@@ -1,19 +1,18 @@
 path = '/var/tmp/.bitbucket.yaml'
 y = YAML.load_file(path) if File.exist? path
 
-# facter lib/facter/util/file_read.rb // does not work
-
+# facter lib/facter/util/file_read.rb // does not work - Error: Facter: error while resolving custom fact "bitbucket_setup": uninitialized constant Facter::Util::FileRead
 # Fact to work around PUP-1125 requirement.
-Facter.add(:bitbucket_setup) do
-  setcode do
-    if File.exist? path
-      file = Facter::Util::FileRead.read(y['properties_file'])
-      file =~ /removed by unattended setup on/ ? true : false
-    else
-      false
-    end
-  end
-end
+#Facter.add(:bitbucket_setup) do
+#  setcode do
+#    if File.exist? path
+#      file = Facter::Util::FileRead.read(y['properties_file'])
+#      file =~ /removed by unattended setup on/ ? true : false
+#    else
+#      false
+#    end
+#  end
+#end
 
 # Fact to work around PUP-1125 requirement.
 Facter.add(:bitbucket_port) do

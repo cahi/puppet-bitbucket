e19991051aa5118472b9074b4f55ca0a0b01dc2e
Carsten Hilber <chilber@agile-im.de>
2017-02-28 11:56:27 +0100

    commented facter

--- lib/facter/bitbucket_setup.rb
+++ lib/facter/bitbucket_setup.rb
@@ -1,7 +1,7 @@
 path = '/var/tmp/.bitbucket.yaml'
 y = YAML.load_file(path) if File.exist? path
 
-facter lib/facter/util/file_read.rb
+# facter lib/facter/util/file_read.rb // does not work
 
 # Fact to work around PUP-1125 requirement.
 Facter.add(:bitbucket_setup) do
